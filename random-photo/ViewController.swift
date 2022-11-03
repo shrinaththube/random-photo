a3f4f575e01e0884e93675420a4a1bb3608a0b84
Shrinath-Thube <shrinath.thube@ibm.com>
2022-11-02 18:01:38 -0700

    Initial Commit

---
+++ random-photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  random-photo
+//
+//  Created by shrinath thube on 11/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
